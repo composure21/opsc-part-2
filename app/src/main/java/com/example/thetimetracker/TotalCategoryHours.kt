56962101305011f9701199f92aa2595c09609854
ST10083465 <ST10083465@vcconnect.edu.za>
2024-04-30 14:25:02 +0200

    push

---
+++ app/src/main/java/com/example/thetimetracker/TotalCategoryHours.kt
@@ -0,0 +1,11 @@
+package com.example.thetimetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TotalCategoryHours : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_total_category_hours)
+    }
+}
