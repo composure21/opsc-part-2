84999c0255f8425263bf411b855a43b617ce1e93
ST10083465 <ST10083465@vcconnect.edu.za>
2024-04-30 09:50:08 +0200

    First commit

---
+++ app/src/main/java/com/example/thetimetracker/DailyGoalEntries.kt
@@ -0,0 +1,11 @@
+package com.example.thetimetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DailyGoalEntries : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily_goal_entries)
+    }
+}
---
+++ app/src/main/java/com/example/thetimetracker/DailyGoals.kt
@@ -0,0 +1,11 @@
+package com.example.thetimetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DailyGoals : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily_goals)
+    }
+}
