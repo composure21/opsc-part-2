3c4c1d2e93e2c3e1c29104402bd5662042d23a7b
ST10083465 <ST10083465@vcconnect.edu.za>
2024-05-01 14:10:22 +0200

    push

--- app/src/main/java/com/example/thetimetracker/DailyGoalEntries.kt
+++ app/src/main/java/com/example/thetimetracker/DailyGoalEntries.kt
@@ -2,10 +2,17 @@ package com.example.thetimetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class DailyGoalEntries : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_goal_entries)
+
+        val backBtn2 = findViewById<LinearLayout>(R.id.btnBack2)
+
+        backBtn2.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/thetimetracker/EntriesMade.kt
+++ app/src/main/java/com/example/thetimetracker/EntriesMade.kt
@@ -2,10 +2,17 @@ package com.example.thetimetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class EntriesMade : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entries_made)
+
+        val backBtn = findViewById<LinearLayout>(R.id.btnBack1)
+
+        backBtn.setOnClickListener {
+            finish()
+        }
     }
 }
